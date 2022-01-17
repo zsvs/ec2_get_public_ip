f5427f5117df5adb6b5f334ff1f35c321bf9c95b
Valerii Stepanets <62026632+zsvs@users.noreply.github.com>
2022-01-17 15:26:09 +0200

    Script
    Gets Public IPv4 address from AWS EC2

---
+++ GetEC2.py
@@ -0,0 +1,28 @@
+﻿import boto3
+import sys
+
+
+
+TagName = sys.argv[1]#"Env"
+TagValue = sys.argv[2]#"Test"
+
+ec2 = boto3.client('ec2')
+
+def GetIPv4Add(InstanceList):
+    return InstanceList["NetworkInterfaces"][0]["Association"]["PublicIp"]
+
+def GetHosts():
+    Addresses = list()
+    responce = ec2.describe_instances(Filters=[
+                {
+                   "Name": "tag:{0}".format(TagName),
+                    "Values": ["{0}".format(TagValue)]
+            }
+        ])
+
+    for InstanceCount in range(0, len(responce["Reservations"]), 1):
+       for Key in responce["Reservations"][InstanceCount]["Instances"]:
+          Addresses.append(GetIPv4Add(Key))
+    return Addresses
+
+print(GetHosts())
