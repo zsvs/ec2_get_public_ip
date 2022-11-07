6985c37740dff97eaa9cc9540abc0ec50deed59e
Valerii Stepanets <62026632+zsvs@users.noreply.github.com>
2022-11-07 18:10:00 +0200

    Update GetEC2.py

--- GetEC2.py
+++ GetEC2.py
@@ -1,47 +1,47 @@
-﻿import boto3
+import boto3
 import sys
 import re
 import pathlib
 
-TagName = sys.argv[1]
-TagValue = sys.argv[2]
+tag_name = sys.argv[1]
+tag_value = sys.argv[2]
 
 ec2 = boto3.client('ec2')
 AWSAMI = boto3.resource("ec2")
 
 responce = ec2.describe_instances(Filters=[
                 {
-                   "Name": "tag:{0}".format(TagName),
-                    "Values": ["{0}".format(TagValue)]
+                   "Name": f"tag:{tag_name}",
+                    "Values": [f"{tag_value}"]
             }
         ])
 
-InstanceNumber = len(responce["Reservations"]) # All instances number
+instance_number = len(responce["Reservations"]) # All instances number
 
-def GetSSHNameByAWSImageName():
-    for EC2InstanceNumber in range(0,InstanceNumber):
-        if re.search("(ubuntu)", AWSAMI.Image(responce["Reservations"][EC2InstanceNumber]["Instances"][0]["ImageId"]).name):
+def get_ssh_name_by_AWS_image_name():
+    for EC2instance_number in range(0,instance_number):
+        if re.search("(ubuntu)", AWSAMI.Image(responce["Reservations"][EC2instance_number]["Instances"][0]["ImageId"]).name):
             return "ubuntu"
-        elif re.search("(amazon)", AWSAMI.Image(responce["Reservations"][EC2InstanceNumber]["Instances"][0]["ImageId"]).name):
+        elif re.search("(amazon)", AWSAMI.Image(responce["Reservations"][EC2instance_number]["Instances"][0]["ImageId"]).name):
             return "ec2-user"
 
-print("Number of EC2: ", InstanceNumber)
+print("Number of EC2: ", instance_number)
 
-def GetIPv4Add(InstanceList):
-    return InstanceList["NetworkInterfaces"][0]["Association"]["PublicIp"]
+def get_IPv4Addr(instance_list):
+    return instance_list["NetworkInterfaces"][0]["Association"]["PublicIp"]
 
-def GetHosts():
+def get_hosts():
     Addresses = list()
-    for InstanceCount in range(0, InstanceNumber):
-        for Key in responce["Reservations"][InstanceCount]["Instances"]:
-           Addresses.append(GetIPv4Add(Key))
+    for instance_count in range(0, instance_number):
+        for Key in responce["Reservations"][instance_count]["Instances"]:
+           Addresses.append(get_IPv4Addr(Key))
     return Addresses
 
-def GetResult():
+def get_result():
     Pattern = "[awsservershosts]\n"
-    for InstanceCount in range(0, InstanceNumber):
-        Pattern += GetHosts()[InstanceCount] + f" ansible_user={GetSSHNameByAWSImageName()}" + "\n"
+    for instance_count in range(0, instance_number):
+        Pattern += get_hosts()[instance_count] + f" ansible_user={get_ssh_name_by_AWS_image_name()}" + "\n"
     return Pattern
 
 with open(f"{pathlib.Path().resolve()}/hosts", "w") as f:
-    f.write(GetResult())
+    f.write(get_result())
